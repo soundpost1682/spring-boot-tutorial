11017db6a3aac096238b314665447e6e29116652
soundpost1682 <guarneri1682@gmail.com>
2024-07-15 18:33:05 -0700

    3rd !

--- src/main/java/prac/sprb/tutorial/run/RunController.java
+++ src/main/java/prac/sprb/tutorial/run/RunController.java
@@ -1,7 +1,9 @@
 package prac.sprb.tutorial.run;
 
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -29,7 +31,7 @@ public class RunController {
 
         Optional<Run> run = runRepository.findById(id);
         if (run.isEmpty()){
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Run not found");
+            throw new RunNotFoundException();
         }
         return run.get();
     }
@@ -37,14 +39,14 @@ public class RunController {
     // post
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping()
-    void create(@RequestBody Run run){
+    void create(@Valid @RequestBody Run run){
         runRepository.create(run);
     }
 
     // put
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PutMapping("/{id}")
-    void update(@RequestBody Run run, @PathVariable Integer id){
+    void update(@Valid @RequestBody Run run, @PathVariable Integer id){
         runRepository.update(run, id);
     }
 
